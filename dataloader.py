bd1e727af8be92f3729ee89acfad63403d5e737c
JeasunLok <1913587141@qq.com>
2024-04-03 20:34:14 +0800

    add: dataloader

--- dataloader.py
+++ dataloader.py
@@ -27,3 +27,33 @@ class MyDataset(Dataset):
     def __len__(self):
         return self.length
 
+class SegDataset(Dataset):
+    def __init__(self, annotation_lines, input_shape, num_classes, image_transform=None, label_transform=None):
+        self.annotation_lines = annotation_lines
+        self.length = len(annotation_lines)
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+        self.image_transform = image_transform
+        self.label_transform = label_transform
+
+    def __getitem__(self, index):
+        annotation_line = self.annotation_lines[index]
+        name_label = annotation_line.split()[0]
+        name_image = name_label.replace("label", "image").split(".")[0] + ".png"
+
+        image = Image.open(name_image)
+        label = Image.open(name_label)
+
+        if self.image_transform is not None:
+            image = self.image_transform(image)
+        else:
+            image = torch.from_numpy(np.transpose(np.array(image), [2, 0 ,1]))
+        if self.label_transform is not None:
+            label = self.label_transform(label)*255
+            label = torch.squeeze(label, 0)
+        else:
+            label= torch.from_numpy(np.array(label))*255
+        return image, label
+
+    def __len__(self):
+        return self.length
