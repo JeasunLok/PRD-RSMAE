66a8727ed0ad208bce3bc7318472294428568d18
JeasunLok <1913587141@qq.com>
2024-04-05 19:04:52 +0800

    fix: some details

--- train_segmentor.py
+++ train_segmentor.py
@@ -18,12 +18,16 @@ from torch.cuda.amp import GradScaler
 
 #-------------------------------------------------------------------------------
 # train model
-def train_epoch(model, train_loader, criterion, optimizer, e, epoch, device, num_classes, scaler, fp16):
+def train_epoch(model, train_loader, criterion, optimizer, e, epoch, device, num_classes, scaler, fp16, ignore_index=None):
     loss_show = AverageMeter()
+    local_rank = int(os.environ["LOCAL_RANK"])
     acc = 0
     mIoU = 0
     CM = np.zeros([num_classes, num_classes])
-    loop = tqdm(enumerate(train_loader), total = len(train_loader))
+    if local_rank == 0:
+        loop = tqdm(enumerate(train_loader), total = len(train_loader))
+    else:
+        loop = enumerate(train_loader)
     for batch_idx, (batch_data, batch_label) in loop:
         batch_data = batch_data.to(device).float()
         batch_label = batch_label.to(device).long()
@@ -32,14 +36,14 @@ def train_epoch(model, train_loader, criterion, optimizer, e, epoch, device, num
 
         if fp16:
             with torch.cuda.amp.autocast():
-                batch_prediction = model(batch_data)
+                batch_prediction, out, embedding = model(batch_data)
                 loss = criterion(batch_prediction, batch_label)
 
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
         else:
-            batch_prediction = model(batch_data)
+            batch_prediction, out, embedding = model(batch_data)
             loss = criterion(batch_prediction, batch_label)
             loss.backward()
             optimizer.step()     
@@ -53,31 +57,36 @@ def train_epoch(model, train_loader, criterion, optimizer, e, epoch, device, num
 
         # update the loss and the accuracy 
         loss_show.update(loss.data, n)
-        acc = compute_acc(CM)
-        mIoU = compute_mIoU(CM)
+        acc = compute_acc(CM, ignore_index=ignore_index)
+        mIoU = compute_mIoU(CM, ignore_index=ignore_index)
 
-        loop.set_description(f'Train Epoch [{e+1}/{epoch}]')
-        loop.set_postfix({"train_loss":loss_show.average.item(),
-                          "train_accuracy": str(round(acc*100, 2)) + "%",
-                          "train_mIoU": str(round(mIoU*100, 2)) + "%"})
+        if local_rank == 0:
+            loop.set_description(f'Train Epoch [{e+1}/{epoch}]')
+            loop.set_postfix({"train_loss":loss_show.average.item(),
+                            "train_accuracy": str(round(acc*100, 2)) + "%",
+                            "train_mIoU": str(round(mIoU*100, 2)) + "%"})
 
     return acc, mIoU, loss_show.average.item()
 #-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
 # valid model
-def valid_epoch(model, val_loader, criterion, e, epoch, device, num_classes):
+def valid_epoch(model, val_loader, criterion, e, epoch, device, num_classes, ignore_index=None):
     loss_show = AverageMeter()
+    local_rank = int(os.environ["LOCAL_RANK"])
     acc = 0
     mIoU = 0
     CM = np.zeros([num_classes, num_classes])
-    loop = tqdm(enumerate(val_loader), total = len(val_loader))
+    if local_rank == 0:
+        loop = tqdm(enumerate(val_loader), total = len(val_loader))
+    else:
+        loop = enumerate(val_loader)
     with torch.no_grad():
         for batch_idx, (batch_data, batch_label) in loop:
             batch_data = batch_data.to(device).float()
             batch_label = batch_label.to(device).long()
 
-            batch_prediction = model(batch_data)
+            batch_prediction, out, embedding = model(batch_data)
             loss = criterion(batch_prediction, batch_label)
 
             batch_prediction = F.softmax(batch_prediction, dim=1)
@@ -89,29 +98,35 @@ def valid_epoch(model, val_loader, criterion, e, epoch, device, num_classes):
 
             # update the loss and the accuracy 
             loss_show.update(loss.data, n)
-            acc = compute_acc(CM)
-            mIoU = compute_mIoU(CM)
+            acc = compute_acc(CM, ignore_index=ignore_index)
+            mIoU = compute_mIoU(CM, ignore_index=ignore_index)
 
-            loop.set_description(f'Val Epoch [{e+1}/{epoch}]')
-            loop.set_postfix({"val_loss":loss_show.average.item(),
-                            "val_accuracy": str(round(acc*100, 2)) + "%",
-                            "val_mIoU": str(round(mIoU*100, 2)) + "%"})
+            if local_rank == 0:
+                loop.set_description(f'Val Epoch [{e+1}/{epoch}]')
+                loop.set_postfix({"val_loss":loss_show.average.item(),
+                                "val_accuracy": str(round(acc*100, 2)) + "%",
+                                "val_mIoU": str(round(mIoU*100, 2)) + "%"})
 
     return CM, acc, mIoU, loss_show.average.item()
+#-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
 # test model
-def test_epoch(model, test_loader, device, num_classes):
+def test_epoch(model, test_loader, device, num_classes, ignore_index=None):
+    local_rank = int(os.environ["LOCAL_RANK"])
     acc = 0
     mIoU = 0
     CM = np.zeros([num_classes, num_classes])
-    loop = tqdm(enumerate(test_loader), total = len(test_loader))
+    if local_rank == 0:
+        loop = tqdm(enumerate(test_loader), total = len(test_loader))
+    else:
+        loop = enumerate(test_loader)
     with torch.no_grad():
         for batch_idx, (batch_data, batch_label) in loop:
             batch_data = batch_data.to(device).float()
             batch_label = batch_label.to(device).long()
 
-            batch_prediction = model(batch_data)
+            batch_prediction, out, embedding = model(batch_data)
 
             batch_prediction = F.softmax(batch_prediction, dim=1)
             batch_prediction = torch.argmax(batch_prediction, dim=1)
@@ -121,12 +136,13 @@ def test_epoch(model, test_loader, device, num_classes):
             CM = CM + CM_batch
 
             # update the loss and the accuracy 
-            acc = compute_acc(CM)
-            mIoU = compute_mIoU(CM)
+            acc = compute_acc(CM, ignore_index=ignore_index)
+            mIoU = compute_mIoU(CM, ignore_index=ignore_index)
 
-            loop.set_description(f'Test Epoch')
-            loop.set_postfix({"test_accuracy": str(round(acc*100, 2)) + "%",
-                              "test_mIoU": str(round(mIoU*100, 2)) + "%"})
+            if local_rank == 0:
+                loop.set_description(f'Test Epoch')
+                loop.set_postfix({"test_accuracy": str(round(acc*100, 2)) + "%",
+                                "test_mIoU": str(round(mIoU*100, 2)) + "%"})
 
     return CM, acc, mIoU
 #-------------------------------------------------------------------------------
@@ -138,22 +154,25 @@ if __name__ == "__main__":
     distributed = True
     sync_bn = True
     fp16 = True
+    test = True
 
     num_classes = 9
 
-    model_pretrained = False
-    model_path = r""
+    model_pretrained = True
+    model_path = r"checkpoints/segmentation/2024-04-05-15-35-12/model_state_dict_loss0.1632_epoch70.pth"
     encoder_pretrained = True
-    encoder_path = r""
+    encoder_path = r"checkpoints/PRD289K/vit-b-mae-200.pt"
+    freeze_encoder = False
     
 
     input_shape = [512, 512]
     epoch = 100
-    save_period = 5
-    batch_size = 48
+    save_period = 10
+    batch_size = 96
+    ignore_index = 0 # None
 
     # 学习率
-    lr = 5e-4
+    lr = 1e-3
     min_lr = lr*0.01
 
     # 优化器
@@ -161,18 +180,20 @@ if __name__ == "__main__":
     weight_decay = 0
     
     data_dir = r"data/segmentation"
-    logs_dir = r"logs"
-    checkpoints_dir = r"checkpoints"
+    logs_dir = r"logs/segmentation"
+    checkpoints_dir = r"checkpoints/segmentation"
     time_now = time.localtime()
     logs_folder = os.path.join(logs_dir, time.strftime("%Y-%m-%d-%H-%M-%S", time_now))
     checkpoints_folder = os.path.join(checkpoints_dir, time.strftime("%Y-%m-%d-%H-%M-%S", time_now))
-    os.makedirs(logs_folder)
-    os.makedirs(checkpoints_folder)
+    local_rank = int(os.environ["LOCAL_RANK"])
+    if local_rank == 0:
+        os.makedirs(logs_folder)
+        os.makedirs(checkpoints_folder)
 
     dice_loss = False
     focal_loss = False
-
-    print("===============================================================================")
+    if local_rank == 0:
+        print("===============================================================================")
     # 设置用到的显卡
     ngpus_per_node  = torch.cuda.device_count()
     if Cuda:
@@ -187,24 +208,18 @@ if __name__ == "__main__":
         device = torch.device("cpu")
         local_rank = 0
 
-    model = MAE_Encoder(image_size=input_shape[0], patch_size=32)
+    model = MAE_ViT(image_size=input_shape[0], patch_size=32)
+    
     if encoder_pretrained:
-        model.load_state_dict(torch.load(encoder_path)) 
+        if local_rank == 0:
+            print('Load weights {}.'.format(encoder_path))
+        ddp_model = torch.load(encoder_path)
+        state_dict = ddp_model.state_dict()
+        encoder_state_dict = {k.replace("module.", ""): v for k, v in state_dict.items() if "encoder" in k}
+        model.load_state_dict(encoder_state_dict, strict=False)
+
     model = ViT_Segmentor(model.encoder, out_channels=num_classes, downsample_factor=16)
-
-    # 混精度
-    if fp16:
-        from torch.cuda.amp import GradScaler as GradScaler
-        scaler = GradScaler()
-    else:
-        scaler = None
-
     model_train = model.train()
-    if sync_bn and ngpus_per_node > 1 and distributed:
-        model_train = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model_train)
-    elif sync_bn:
-        print("Sync_bn is not support in one gpu or not distributed.")
-
     if Cuda:
         if distributed:
             model_train = model_train.cuda(local_rank)
@@ -214,8 +229,22 @@ if __name__ == "__main__":
             cudnn.benchmark = True
             model_train = model_train.cuda()
 
+    # 混精度
+    if fp16:
+        from torch.cuda.amp import GradScaler as GradScaler
+        scaler = GradScaler()
+    else:
+        scaler = None
+
+    if sync_bn and ngpus_per_node > 1 and distributed:
+        model_train = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model_train)
+    elif sync_bn:
+        if local_rank == 0:
+            print("Sync_bn is not support in one gpu or not distributed.")
+
     if model_pretrained and model_path != '':
-        print('Load weights {}.'.format(model_path))
+        if local_rank == 0:
+            print('Load weights {}.'.format(model_path))
         model_train.load_state_dict(torch.load(model_path))  
         
     with open(os.path.join(data_dir, r"list/train.txt"),"r") as f:
@@ -231,13 +260,37 @@ if __name__ == "__main__":
     torch.manual_seed(3407)
     np.random.seed(3407)
 
-    print("device:", device, "num_train:", num_train, "num_val:", num_val, "num_test:", num_test)
-    print("===============================================================================")
+    if local_rank == 0:
+        print("device:", device, "num_train:", num_train, "num_val:", num_val, "num_test:", num_test)
+        print("===============================================================================")
 
     # 优化器
-    optimizer = torch.optim.Adam(model_train.parameters(), lr=lr, betas=(momentum, 0.999), weight_decay=weight_decay)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=epoch//10, gamma=0.9) 
-    criterion = nn.CrossEntropyLoss(ignore_index=0).cuda()
+    if freeze_encoder:
+        for param in [model_train.cls_token, model_train.pos_embedding, model_train.patchify, model_train.transformer, model_train.layer_norm]:
+            param.requires_grad = False
+        parameters_to_optimize = [
+            {'params': [model_train.alpha, model_train.beta]},  # 将 alpha 和 beta 参数添加到优化器中
+            {'params': model_train.decoder_1.parameters()},  # 将 decoder_1 的参数添加到优化器中
+            {'params': model_train.decoder_2.parameters()},  # 将 decoder_2 的参数添加到优化器中
+            {'params': model_train.decoder_3.parameters()},  # 将 decoder_3 的参数添加到优化器中
+            {'params': model_train.decoder_4.parameters()},  # 将 decoder_4 的参数添加到优化器中
+            {'params': model_train.decoder_5.parameters()},  # 将 decoder_5 的参数添加到优化器中
+            {'params': model_train.scene_1.parameters()},  # 将 scene_1 的参数添加到优化器中
+            {'params': model_train.scene_2.parameters()},  # 将 scene_2 的参数添加到优化器中
+            {'params': model_train.scene_3.parameters()},  # 将 scene_3 的参数添加到优化器中
+            {'params': model_train.scene_4.parameters()},  # 将 scene_4 的参数添加到优化器中
+            {'params': model_train.scene_5.parameters()},  # 将 scene_5 的参数添加到优化器中
+            {'params': model_train.origin_scene_embedding.parameters()},  # 将 origin_scene_embedding 的参数添加到优化器中
+        ]
+        optimizer = torch.optim.Adam(parameters_to_optimize, lr=lr, betas=(momentum, 0.999), weight_decay=weight_decay)
+    else:
+        optimizer = torch.optim.Adam(model_train.parameters(), lr=lr, betas=(momentum, 0.999), weight_decay=weight_decay)
+
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=epoch//10, gamma=0.9)
+    if ignore_index is not None: 
+        criterion = nn.CrossEntropyLoss(ignore_index=ignore_index).cuda()
+    else: 
+        criterion = nn.CrossEntropyLoss().cuda()
 
     image_transform = get_transform(input_shape, IsResize=True, IsTotensor=True, IsNormalize=True)
     label_transform = get_transform(input_shape, IsResize=True, IsTotensor=True, IsNormalize=False)
@@ -262,44 +315,54 @@ if __name__ == "__main__":
     test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size, num_workers=num_workers, pin_memory=True, drop_last=True, sampler=test_sampler)
 
     # 开始模型训练
-    print("start training")
-    epoch_result = np.zeros([4, epoch])
-    for e in range(epoch):
-        model_train.train()
-        train_acc, train_mIoU, train_loss = train_epoch(model_train, train_loader, criterion, optimizer, e, epoch, device, num_classes, scaler, fp16)
-        scheduler.step()
-        print("Epoch: {:03d} | train_loss: {:.4f} | train_acc: {:.2f}% | train_mIoU: {:.2f}%".format(e+1, train_loss, train_acc*100, train_mIoU*100))
-        epoch_result[0][e], epoch_result[1][e], epoch_result[2][e], epoch_result[3][e]= e+1, train_loss, train_acc*100, train_mIoU*100
+    if not test:
+        if local_rank == 0:
+            print("start training")
+        epoch_result = np.zeros([4, epoch])
+        for e in range(epoch):
+            model_train.train()
+            train_acc, train_mIoU, train_loss = train_epoch(model_train, train_loader, criterion, optimizer, e, epoch, device, num_classes, scaler, fp16, ignore_index=ignore_index)
+            scheduler.step()
+            if local_rank == 0:
+                print("Epoch: {:03d} | train_loss: {:.4f} | train_acc: {:.2f}% | train_mIoU: {:.2f}%".format(e+1, train_loss, train_acc*100, train_mIoU*100))
+            epoch_result[0][e], epoch_result[1][e], epoch_result[2][e], epoch_result[3][e]= e+1, train_loss, train_acc*100, train_mIoU*100
 
-        if ((e+1) % save_period == 0) | (e == epoch - 1):
-            print("===============================================================================")
-            print("start validating")
-            model_train.eval()      
-            val_CM, val_acc, val_mIoU, val_loss = valid_epoch(model_train, val_loader, criterion, e, epoch, device, num_classes)
-            val_weighted_recall, val_weighted_precision, val_weighted_f1 = compute_metrics(val_CM)
-            if (e != epoch -1):
-                print("Epoch: {:03d}  =>  Accuracy: {:.2f}% | MIoU: {:.2f}% | W-Recall: {:.4f} | W-Precision: {:.4f} | W-F1: {:.4f}".format(e+1, val_acc*100, val_mIoU*100, val_weighted_recall, val_weighted_precision, val_weighted_f1))
-            torch.save(model_train, os.path.join(checkpoints_folder, "model_loss" + str(round(val_loss, 4)) + "_epoch" + str(e+1) + ".pth"))
-            torch.save(model_train.state_dict(), os.path.join(checkpoints_folder, "model_state_dict_loss" + str(round(val_loss, 4)) + "_epoch" + str(e+1) + ".pth"))
-            print("===============================================================================")
-    
-    if distributed:
-        train_sampler.set_epoch(epoch)
+            if ((e+1) % save_period == 0) | (e == epoch - 1):
+                if local_rank == 0:
+                    print("===============================================================================")
+                    print("start validating")
+                model_train.eval()      
+                val_CM, val_acc, val_mIoU, val_loss = valid_epoch(model_train, val_loader, criterion, e, epoch, device, num_classes, ignore_index=ignore_index)
+                val_weighted_recall, val_weighted_precision, val_weighted_f1 = compute_metrics(val_CM, ignore_index=ignore_index)
+                if (e != epoch -1):
+                    if local_rank == 0:
+                        print("Epoch: {:03d}  =>  Accuracy: {:.2f}% | MIoU: {:.2f}% | W-Recall: {:.4f} | W-Precision: {:.4f} | W-F1: {:.4f}".format(e+1, val_acc*100, val_mIoU*100, val_weighted_recall, val_weighted_precision, val_weighted_f1))
+                if local_rank == 0:
+                    torch.save(model_train, os.path.join(checkpoints_folder, "model_loss" + str(round(val_loss, 4)) + "_epoch" + str(e+1) + ".pth"))
+                    torch.save(model_train.state_dict(), os.path.join(checkpoints_folder, "model_state_dict_loss" + str(round(val_loss, 4)) + "_epoch" + str(e+1) + ".pth"))
+                    print("===============================================================================")
+        
+        if distributed:
+            train_sampler.set_epoch(epoch)
 
-    if distributed:
-        dist.barrier()
+        if distributed:
+            dist.barrier()
 
-    draw_result_visualization(logs_folder, epoch_result)
-    print("save train logs successfully")
-    print("===============================================================================")
+        draw_result_visualization(logs_folder, epoch_result)
+        if local_rank == 0:
+            print("save train logs successfully")
 
-    print("start testing")
+    if local_rank == 0:
+        print("===============================================================================")
+        print("start testing")
     model_train.eval()
-    test_CM, test_acc, test_mIoU, val_loss = test_epoch(model_train, val_loader, device)
-    test_weighted_recall, test_weighted_precision, test_weighted_f1 = compute_metrics(test_CM)
-    print("Test Result  =>  Accuracy: {:.2f}%| mIoU: {:.2f} | W-Recall: {:.4f} | W-Precision: {:.4f} | W-F1: {:.4f}".format(test_acc*100, test_mIoU*100, test_weighted_recall, test_weighted_precision, test_weighted_f1))
+    test_CM, test_acc, test_mIoU = test_epoch(model_train, test_loader, device, num_classes, ignore_index=ignore_index)
+    test_weighted_recall, test_weighted_precision, test_weighted_f1 = compute_metrics(test_CM, ignore_index)
+    if local_rank == 0:
+        print("Test Result  =>  Accuracy: {:.2f}%| mIoU: {:.2f}% | W-Recall: {:.4f} | W-Precision: {:.4f} | W-F1: {:.4f}".format(test_acc*100, test_mIoU*100, test_weighted_recall, test_weighted_precision, test_weighted_f1))
     store_result(logs_folder, test_acc, test_mIoU, test_weighted_recall, test_weighted_precision, test_weighted_f1, test_CM, epoch, batch_size, lr, weight_decay)
-    print("save test result successfully")
-    print("===============================================================================") 
+    if local_rank == 0:
+        print("save test result successfully")
+        print("===============================================================================") 
 
-
+# torchrun --nproc_per_node=3 train_segmentor.py
