ee296be4cab43f20cd10d3f40ed50e824854f30b
JeasunLok <1913587141@qq.com>
2024-05-22 00:55:27 +0800

    add: SDdataset

--- dataloader.py
+++ dataloader.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 from PIL import Image
 from torch.utils.data.dataset import Dataset
+from torch.utils.data.dataloader import DataLoader
 from utils import *
 
 class MyDataset(Dataset):
@@ -27,6 +28,33 @@ class MyDataset(Dataset):
     def __len__(self):
         return self.length
 
+class SDDataset(Dataset):
+    def __init__(self, annotation_lines, input_shape, image_transform=None):
+        self.annotation_lines = annotation_lines
+        self.length = len(annotation_lines)
+        self.input_shape = input_shape
+        self.image_transform = image_transform
+
+    def __getitem__(self, index):
+        annotation_line = self.annotation_lines[index]
+        name_image = annotation_line.split()[0]
+        image = Image.open(name_image)
+
+        if self.image_transform is not None:
+            image = self.image_transform(image)
+        else:
+            image = torch.from_numpy(np.transpose(np.array(image), [2, 1 ,0]))
+        
+        img_np = np.array(image.squeeze(0))
+        img_hsv = torch.from_numpy(rgb_to_hsv(img_np)[:, :, 2]).unsqueeze(0)
+        img_edge = torch.from_numpy(detect_edges_with_laplacian_and_otsu(img_np)).unsqueeze(0)
+        img_edge = torch.cat([img_edge] * 3, dim=0).byte()
+
+        return image, img_hsv, img_edge
+    
+    def __len__(self):
+        return self.length
+    
 class SegDataset(Dataset):
     def __init__(self, annotation_lines, input_shape, num_classes, image_transform=None, label_transform=None):
         self.annotation_lines = annotation_lines
@@ -57,3 +85,34 @@ class SegDataset(Dataset):
 
     def __len__(self):
         return self.length
+
+
+if __name__ == "__main__":
+    annotation_lines = [r"E:\202403\PRD-RSMAE\PRD-RSMAE\F49E001015_Level_17_1009.TIF", r"E:\202403\PRD-RSMAE\PRD-RSMAE\F49E001015_Level_17_1009.TIF"]  # 你需要替换为实际的文件路径  
+    input_shape = [512, 512]  # 假设的输入形状，你可能需要根据你的模型进行调整  
+        
+    # 创建一个SDDataset实例  
+    dataset = SDDataset(annotation_lines, input_shape)  
+        
+    # 创建一个DataLoader来加载数据  
+    data_loader = DataLoader(dataset, batch_size=1, shuffle=False)  
+    # 遍历DataLoader并打印结果  
+    for image, img_hsv, img_edge in data_loader:  
+        print(f"Image shape: {image.shape}")  
+        print(f"HSV shape: {img_hsv.shape}")  
+        print(f"Edge shape: {img_edge.shape}")
+        a = img_edge
+        b = image
+
+    plt.figure()
+    b = np.transpose(np.array(b.squeeze(0)))*255
+    print(b.shape)
+    plt.imshow(b, cmap='gray')
+    plt.show()
+    
+    plt.figure()
+    a = np.transpose(np.array(a.squeeze(0)))*255
+    print(a.shape)
+    plt.imshow(a, cmap='gray')
+    plt.show()
+    
